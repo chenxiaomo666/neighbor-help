b347efafbb6ffe97681e734773147a0b92efe101
chenhao <13784197113@163.com>
2020-07-30 15:01:51 +0800

    完成现有所有功能

--- neighbor_help/views/message_view.py
+++ neighbor_help/views/message_view.py
@@ -82,11 +82,14 @@ def message_name():
 def linlixiang_release():
     data = request.get_json()
 
-    print(data["img_path"])
-    if data["img_path"][0:4] == "http":
-        img_url = "https://dev.mylwx.cn:5000/file/{}".format(data["img_path"][11:])
-    elif data["img_path"][0:6] == "wxfile":
-        img_url = "https://dev.mylwx.cn:5000/file/{}".format(data["img_path"][9:])
+    try:
+        img_url = ""
+        if data["img_path"][0:4] == "http":
+            img_url = "https://dev.mylwx.cn:5000/file/{}".format(data["img_path"][11:])
+        elif data["img_path"][0:6] == "wxfile":
+            img_url = "https://dev.mylwx.cn:5000/file/{}".format(data["img_path"][9:])
+    except:
+        img_url = ""
 
     message = Message()
     message.user_id = data["user_id"]
@@ -132,17 +135,44 @@ def linlixiang_list():
     })
 
 
+@message_view.route("/linlixiang/single", methods=["GET"])
+@panic()
+def linlixiang_single():
+    message_id = request.args.get("message_id")
+
+    result = {}
+    message = base_query(Message).filter_by(id=message_id).first()
+    if message is not None:
+        result["user_info"] = get_user_info(message.user_id)
+        result["time"] = str(message.time)
+        if message.message_name == "物品共享":
+            result["title"] = message.message_name
+            result["name"] = message.title
+            result["img_url"] = message.content
+        elif message.message_name == "车位共享":
+            result["title"] = message.message_name
+            result["avail_time"] = message.title
+            result["img_url"] = message.content
+
+    return success({
+        "result": result
+    })
+
+
 # ###########需修改  # 已修改
 @message_view.route("/linlidai/release", methods=["POST"])
 @panic()
 def linlidai_release():
     data = request.get_json()
 
-    print(data["img_path"])
-    if data["img_path"][0:4] == "http":
-        img_url = "https://dev.mylwx.cn:5000/file/{}".format(data["img_path"][11:])
-    elif data["img_path"][0:6] == "wxfile":
-        img_url = "https://dev.mylwx.cn:5000/file/{}".format(data["img_path"][9:])
+    try:
+        img_url = ""
+        if data["img_path"][0:4] == "http":
+            img_url = "https://dev.mylwx.cn:5000/file/{}".format(data["img_path"][11:])
+        elif data["img_path"][0:6] == "wxfile":
+            img_url = "https://dev.mylwx.cn:5000/file/{}".format(data["img_path"][9:])
+    except:
+        img_url = ""
 
     content = {}
     content["img_url"] = img_url
@@ -219,11 +249,14 @@ def linlidai_single():
 def linliyue_release():
     data = request.get_json()
 
-    print(data["img_path"])
-    if data["img_path"][0:4] == "http":
-        img_url = "https://dev.mylwx.cn:5000/file/{}".format(data["img_path"][11:])
-    elif data["img_path"][0:6] == "wxfile":
-        img_url = "https://dev.mylwx.cn:5000/file/{}".format(data["img_path"][9:])
+    try:
+        img_url = ""
+        if data["img_path"][0:4] == "http":
+            img_url = "https://dev.mylwx.cn:5000/file/{}".format(data["img_path"][11:])
+        elif data["img_path"][0:6] == "wxfile":
+            img_url = "https://dev.mylwx.cn:5000/file/{}".format(data["img_path"][9:])
+    except:
+        img_url = ""
 
     content = {}
     content["img_url"] = img_url
@@ -291,6 +324,163 @@ def linliyue_single():
         result["two_input"] = content["two_input"]
         result["three_input"] = content["three_input"]
         result["time"] = str(message.time)
+
+    return success({
+        "result": result
+    })
+
+
+##########################
+@message_view.route("/dynamic/release", methods=["POST"])
+@panic()
+def dynamic_release():
+    data = request.get_json()
+
+    print(data)
+
+    try:
+        img_url = ""
+        if data["img_path"][0:4] == "http":
+            img_url = "https://dev.mylwx.cn:5000/file/{}".format(data["img_path"][11:])
+        elif data["img_path"][0:6] == "wxfile":
+            img_url = "https://dev.mylwx.cn:5000/file/{}".format(data["img_path"][9:])
+    except:
+        img_url = ""
+
+    content = {}
+    content["img_url"] = img_url
+    content["content"] = data["content"]
+
+    message = Message()
+    message.user_id = data["user_id"]
+    message.message_type = 9    # 邻里动态
+    message.message_name = "邻里动态"
+    # message.title = data["title"]
+    message.content = json.dumps(content)
+    message.time = datetime.now()
+
+    db.session.add(message)
+    db.session.commit()
+
+    return success()
+
+
+@message_view.route("/dynamic/list", methods=["GET"])
+@panic()
+def dynamic_list():
+    
+    result = []
+    messages = base_query(Message).filter_by(message_type=9).order_by(Message.time.desc()).all()
+    for message in messages:
+        content = json.loads(message.content)
+        result.append({
+            "id": message.id,
+            "user_info": get_user_info(message.user_id),
+            "img_url": content["img_url"],
+            "content": content["content"],
+            "time": str(message.time)
+        })
+
+    return success({
+        "result": result
+    })
+
+
+# 给用户发消息
+@message_view.route("/news/send", methods=["POST"])
+@panic()
+def news_send():
+
+    data = request.get_json()
+    from_user = get_user_info(data["user_id"])
+    target_user = get_user_info(data["target_user_id"])
+    print(data)
+    message_time = data["time"]
+
+    if data["type"] == "邻里约":
+        news_content = "{}向您在邻里约中{}时提出的标题为{}，内容为{}的活动发起了加入请求".format(from_user["name"], message_time,  data["title"], data["content"])
+    elif data["type"] == "邻里代":
+        news_content = "{}向您在邻里代中{}时提出的地址为{}，代购内容为{}的请求发起了接单".format(from_user["name"], message_time, data["address"], data["mass_list"])
+    elif data["type"] == "邻里享":
+        if data["title"] == "物品共享":
+            news_content = "{}向您在邻里享中{}时分享的名为{}的物品很感兴趣，提出了申请".format(from_user["name"], message_time, data["name"])
+        elif data["title"] == "车位共享":
+            news_content = "{}向您在邻里享中{}时分享的，可用时间在{}的车位很感兴趣，提出了申请".format(from_user["name"], message_time, data["avail_time"])
+
+    store_content = {}
+    store_content["news_content"] = news_content
+    store_content["from_user"] = from_user
+    store_content["target_user"] = target_user
+    store_content["message_time"] = message_time
+    store_content["message_id"] = data["message_id"]
+
+    message = Message()
+    message.user_id = data["target_user_id"]
+    message.message_type = 100   # 额，储存该推送消息是否被阅读过
+    message.message_name = "最近消息"
+    message.content = json.dumps(store_content)
+    message.time = datetime.now()
+
+    db.session.add(message)
+    db.session.commit()
+
+    return success()
+
+
+@message_view.route("/news/list", methods=["GET"])
+@panic()
+def news_list():
+    user_id = request.args.get("user_id")
+    is_read = request.args.get("is_read")
+
+    result = []
+    messages = base_query(Message).filter_by(user_id=user_id, message_type=is_read).all()
+    for message in messages:
+        store_content = json.loads(message.content)
+        result.append({
+            "message_id": message.id,
+            "news_content": store_content["news_content"],
+            "time" : message.time,
+            "from_user": store_content["from_user"],
+            "is_read": is_read
+        })
+
+    return success({
+        "result": result
+    })
+
+
+@message_view.route("/news/toread", methods=["post"])
+@panic()
+def news_to_read():
+    data = request.get_json()
+    message_id = data["message_id"]
+    
+    message = base_query(Message).filter_by(id=message_id).first()
+    if message is not None:
+        message.message_type = 101
+
+        db.session.commit()
+        return success()
+    else:
+        return error(reason="message_id错误")
+
+
+@message_view.route("/news/isjoin", methods=["get"])
+@panic()
+def news_is_join():
+    user_id = request.args.get("user_id")
+    message_id = request.args.get("message_id")
+
+    result = 1
+    messages = base_query(Message).filter_by(message_name="最近消息").all()
+    for message in messages:
+        store_content = json.loads(message.content)
+        if (int(user_id) == store_content["from_user"]["user_id"]) and (message_id == store_content["message_id"]):
+            result = 1
+            break
+    else:
+        result = 0
 
     return success({
         "result": result
